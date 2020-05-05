df81fa581028cb9c56b06f8c314391f4a361849b
mkrzytsanek <mkrzystanek@gmail.com>
2020-05-05 10:37:52 +0200

    add problem 6 tests and explanation

---
+++ Problem6/explanation_6.md
@@ -0,0 +1,5 @@
+Time complexity: O(n), as algorithm requires traversal of whole 
+input list.
+
+Space complexity: O(n), as algorithm operates on the input list
+and other variables that do not depend on the input.
--- Problem6/problem_6.py
+++ Problem6/problem_6.py
@@ -10,13 +10,18 @@ def get_min_max(ints):
     Args:
        ints(list): list of integers containing one or more integers
     """
+    if len(ints) == 0:
+        return None, None
+
     min_value = float("inf")
     max_value = float("-inf")
+
     for i in ints:
         if i < min_value:
             min_value = i
         if i > max_value:
             max_value = i
+    
     return min_value, max_value
 
 
@@ -24,5 +29,11 @@ def get_min_max(ints):
 
 l = [i for i in range(0, 10)]  # a list containing 0 - 9
 random.shuffle(l)
+l2 = []
+l3 = [1, -1, 1, 0]
+l4 = [0, 0, 0]
 
-print ("Pass" if ((0, 9) == get_min_max(l)) else "Fail")
+print("Pass" if ((0, 9) == get_min_max(l)) else "Fail")
+print("Pass" if ((None, None) == get_min_max(l2)) else "Fail")
+print("Pass" if ((-1, 1) == get_min_max(l3)) else "Fail")
+print("Pass" if ((0, 0) == get_min_max(l4)) else "Fail")
